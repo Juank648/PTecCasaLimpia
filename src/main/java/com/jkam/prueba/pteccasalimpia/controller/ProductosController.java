48416f534800cfed7f9c64f0f37428de175ba1c0
juank <juank@Asus_PC>
2024-12-25 16:47:09 -0500

    Conectado a Servidor

---
+++ src/main/java/com/jkam/prueba/pteccasalimpia/controller/ProductosController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jkam.prueba.pteccasalimpia.controller;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+/**
+ *
+ * @author juank
+ */
+@Named("productoController")
+@RequestScoped
+public class ProductosController {
+    
+}
